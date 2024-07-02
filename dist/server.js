c3009c1b672b094ed7a85866e76fd90ecd31cb31
Evandro Lugli <evandro.lugli@gmail.com>
2024-07-02 20:25:06 +1000

    build

---
+++ dist/server.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startServer = startServer;
+const fastify_1 = __importDefault(require("fastify"));
+const app = (0, fastify_1.default)();
+const PORT = Number(process.env.PORT || 3000);
+app.get('/', async (request, reply) => {
+    return { message: 'Hello, Heroku with Fastify!' };
+});
+app.get('/prompts', async (request, reply) => {
+    return { message: 'Hello, Prompts!' };
+});
+function startServer(port = PORT) {
+    app.listen({ port, host: '0.0.0.0' }, (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log(`Server is running at ${address}`);
+    });
+}
+startServer();
