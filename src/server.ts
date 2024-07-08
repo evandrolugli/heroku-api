94af41eab0accb7f234a3edbff71fa66595fc88c
Evandro Lugli <evandro.lugli@gmail.com>
2024-07-08 22:31:17 +1000

    uptade postgres

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify';
+import { prisma } from './lib/prisma';
 
 const app = fastify();
 const PORT = Number(process.env.PORT || 3000);
@@ -9,12 +10,16 @@ app.get('/', async (request, reply) => {
 
 app.get('/prompts', async (request, reply) => {
   
-  return { message: 'Hello, Prompts!' };
+  // return { message: 'Hello, Prompts!' };
+  
+  const prompts = await prisma.prompt.findMany()
+
+  return prompts;
 
 });
 
 export function startServer(port = PORT) {
-  app.listen({ port, host: '0.0.0.0' }, (err, address) => {
+  app.listen({ port, host: '127.0.0.1' }, (err, address) => {
     if (err) {
       console.error(err);
       process.exit(1);
